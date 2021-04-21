0c250a852bbd181bfd32e0f47da6853fcef022db
Vituz <vtbattaglia@gmail.com>
2021-04-21 16:04:51 +0200

    Create file main.js

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// writeln
+// document.writeln("infiltrato js");
+
+// alert
+alert("Abbiamo appena iniziato ad usare JS");
+
+// getId
+document.getElementById('stranger').innerHTML = 'Cose Strane Vol.1';
+
+document.getElementById("easy").innerHTML="Un posto tranquillo";
